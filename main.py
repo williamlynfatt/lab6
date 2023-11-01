b37cc144c9d033d87c110166174b9a200745e83b
James <jamesgannon68@gmail.com>
2023-11-01 14:17:12 -0400

    added a decode method

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def main():
             print("Your password has been encoded and stored!")
             print(password)
 
+        if option == "2":
+            decoded = decode(password)
+            print(f"The decoded password is {decoded}, and the original password is {password}")
+
         if option == "3":
             exit()
 def encode(number): #This function encodes the inserted password adding 3 to each except when it is over 10 it returns the last digit
@@ -26,6 +30,23 @@ def encode(number): #This function encodes the inserted password adding 3 to eac
         i += 1
         number//= 10
     return output
+
+def decode(num):
+    result = 0
+    i = 0
+    while num>0:
+        temp = (num%10)-3
+        if temp == -1:
+            temp = 9
+        if temp == -2:
+            temp = 8
+        if temp == -3:
+            temp = 7
+        result += temp*(10**i)
+        i += 1
+        num //= 10
+    return result
+
 
 if __name__ == '__main__':
     main()
